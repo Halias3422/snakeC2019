9f91ddfb8fb89ac31117d048abcaa12b184f1ef8
Halias3422 <v.desaintexupery@hotmail.fr>
2019-07-08 00:30:37 +0200

    corrected addlistback, need to check for impact

--- snake.c
+++ snake.c
@@ -1,5 +1,5 @@
 #include "snake.h"
-
+/*
 t_snake	*add_list_back(t_snake *snake)
 {
 	t_snake *new;
@@ -12,30 +12,49 @@ t_snake	*add_list_back(t_snake *snake)
 	new->prev = snake;
 	return (new);
 }
+*/
+
+t_snake	*add_list_back(t_snake *snake)
+{
+	t_snake	*new;
+
+	new = (t_snake*)malloc(sizeof(t_snake));
+//	new = NULL;
+	new->next = NULL;
+	new->prev = snake;
+	snake->next = new;
+	return(snake);
+}
 
 void	create_snake(t_snake *snake, int y, int x)
 {
 	int	snake_init = 0;
-
+	snake->next = NULL;
+	snake->prev = NULL;
 	while (snake_init++ < 4)
 	{
 		snake->x = x;
 		snake->y = y++;
 		snake = add_list_back(snake);
+		snake = snake->next;
 	}
+	while (snake->prev)
+		snake = snake->prev;
+	endwin();
+	exit(0);
 }
 
 void	print_snake(t_snake *snake, WINDOW *win)
 {
-	int y = 0;
 	while (snake->next)
-	{
+	{	
 		mvwaddch(win, snake->y, snake->x, '#');
 		snake = snake->next;
 	}
 }
 
-void	print_apple(WINDOW *win, t_snake *snake, t_apple *apple, int minx, int maxx, int miny, int maxy)
+
+void	get_apple_position(t_snake *snake, t_apple *apple, int minx, int maxx, int miny, int maxy)
 {
 	t_snake *head;
 	int		error = 0;
@@ -58,72 +77,98 @@ void	print_apple(WINDOW *win, t_snake *snake, t_apple *apple, int minx, int maxx
 		else
 			snake = snake->next;
 	}
+}
+
+void	print_apple(t_apple *apple)
+{
 	move(apple->y, apple->x);
 	printw("@");
 	apple->is_eaten = 0;
 }
 
+int		check_if_snake_eats_apple(t_snake *snake, t_apple *apple)
+{
+	move(0, 0);
+	if (snake->prev->x + ((COLS / 2) / 3) == apple->x && snake->prev->y + ((LINES / 2) / 2) == apple->y)
+	{
+		apple->is_eaten = 1;
+		return (1);
+	}
+	return (0);
+}
 
-t_snake		*delete_first_node_snake(t_snake *tail)
+t_snake		*delete_first_node_snake(t_snake *tail, WINDOW *win)
 {
 	t_snake	*head;
-
-	tail = tail->next;
+	while (tail->prev->prev)
+		tail = tail->prev;
 	head = tail->prev;
+	mvwaddch(win, head->y, head->x, ' ');
 	tail->prev = NULL;
 	free(head);
 	return (tail);
 }
-t_snake		*move_snake_left(t_snake *tail)
-{
-	tail = delete_first_node_snake(tail);
+
+t_snake		*move_snake_left(t_snake *tail, WINDOW *win, t_apple *apple)
+{	
 	while (tail->next)	
 		tail = tail->next;
 	tail->x = tail->prev->x - 1;
 	tail->y = tail->prev->y;
 	tail = add_list_back(tail);
-	while (tail->prev)
-		tail = tail->prev;
+	if (check_if_snake_eats_apple(tail, apple) == 0)
+		tail = delete_first_node_snake(tail, win);
 	return (tail);
 }
 
-t_snake		*move_snake_down(t_snake *tail)
+t_snake		*move_snake_down(t_snake *tail, WINDOW *win, t_apple *apple)
 {
-	tail = delete_first_node_snake(tail);
 	while (tail->next)
 		tail = tail->next;
 	tail->x = tail->prev->x;
 	tail->y = tail->prev->y + 1;
 	tail = add_list_back(tail);
-	while (tail->prev)
-		tail = tail->prev;
+	if (check_if_snake_eats_apple(tail, apple) == 0)
+		tail = delete_first_node_snake(tail, win);
 	return (tail);
 }
 
-t_snake		*move_snake_right(t_snake *tail)
+t_snake		*move_snake_right(t_snake *tail, WINDOW *win, t_apple *apple)
 {
-	tail = delete_first_node_snake(tail);
 	while (tail->next)
 		tail = tail->next;
 	tail->x = tail->prev->x + 1;
 	tail->y = tail->prev->y;
 	tail = add_list_back(tail);
-	while (tail->prev)
-		tail = tail->prev;
+if (check_if_snake_eats_apple(tail, apple) == 0)
+		tail = delete_first_node_snake(tail, win);
 	return (tail);
 }
 
-t_snake		*move_snake_up(t_snake *tail)
+t_snake		*move_snake_up(t_snake *tail, WINDOW *win, t_apple *apple)
 {
-	tail = delete_first_node_snake(tail);
 	while (tail->next)
 		tail = tail->next;
 	tail->x = tail->prev->x;
 	tail->y = tail->prev->y - 1;
 	tail = add_list_back(tail);
-	while (tail->prev)
-		tail = tail->prev;
+	if (check_if_snake_eats_apple(tail, apple) == 0)
+		tail = delete_first_node_snake(tail, win);
 	return (tail);
+}
+
+int		check_is_dead(t_snake *snake) { t_snake	*tail; tail = snake;
+	while (snake->next->next)
+		snake = snake->next;
+	while (tail->next->next->next)
+	{	
+		if (tail->x == snake->x && tail->y == snake->y)
+			return (1);
+		tail = tail->next;
+	}
+	if (snake->x >= ((COLS / 2) / 3) + COLS / 1.5 || snake->y >= ((LINES / 2) / 2) + LINES / 1.5) 
+		return (1);
+	return (0);
 }
 
 int		main(void)
@@ -148,12 +193,13 @@ int		main(void)
 	apple = (t_apple*)malloc(sizeof(t_apple));
 	create_snake(snake, (LINES / 2) / 2, (COLS / 2) / 1.5);
 	apple->is_eaten = 1;
-	nodelay(stdscr, TRUE);
+	//nodelay(stdscr, TRUE);
 	while (is_dead == 0)
 	{
 		print_snake(snake, win);
 		if (apple->is_eaten == 1)
-			print_apple(win, snake, apple, ((COLS / 2) / 3) + 1, (((COLS / 2) / 3) + (COLS / 1.5)) - 1, ((LINES / 2) / 2) + 1, (((LINES / 2) / 2) + LINES / 1.5) - 1);
+			get_apple_position(snake, apple, ((COLS / 2) / 3) + 1, (((COLS / 2) / 3) + (COLS / 1.5)) - 1, ((LINES / 2) / 2) + 1, (((LINES / 2) / 2) + LINES / 1.5) - 1);
+		print_apple(apple);
 		input = getch();
 		last_direction = direction;
 		if (input == KEY_UP && last_direction != 3)
@@ -165,17 +211,17 @@ int		main(void)
 		else if (input == KEY_LEFT && last_direction != 2)
 			direction = 4;
 		move(0, 0);
-		printw("dir = %d last_dir = %d\n", direction, last_direction);
 		if (direction == 1)
-			snake = move_snake_up(snake);
+			snake = move_snake_up(snake, win, apple);
 		else if (direction == 2)
-			snake = move_snake_right(snake);
+			snake = move_snake_right(snake, win, apple);
 		else if (direction == 3)
-			snake = move_snake_down(snake);
+			snake = move_snake_down(snake, win, apple);
 		else if (direction == 4)
-			snake = move_snake_left(snake);	
+			snake = move_snake_left(snake, win, apple);	
+		is_dead = check_is_dead(snake);
+		timeout(150);
 		wrefresh(win);
-		usleep(300000);
 	}
 	getch();
 	endwin();
